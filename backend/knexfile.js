8152ae78cfad5ba6f94272fc53c66c2f3dd7dd9a
Artur <vanivskyi.artur@thinkful.com>
2022-11-26 16:14:15 -0500

    chaged connection in knefile

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -2,13 +2,14 @@ require("dotenv").config();
 const { DATABASE_URL, DATABASE_URL_PRODUCTION } = process.env;
 const path = require("path");
 
+console.log(path)
 
 module.exports = {
   development: {
     client: "postgresql",
     pool: { min: 2, max: 10 },
     connection:
-    DATABASE_URL,
+    "postgres://noqovsgn:0ueYnIUD2CY6iRCSJyLek1UT-r4hGVYa@isilo.db.elephantsql.com/noqovsgn",
     magrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
@@ -19,7 +20,7 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: DATABASE_URL_PRODUCTION,
+    connection: "postgres://tztgcaku:W0lkfUcNc6HGd0nZl4OaFVvZTG-5n89Y@peanut.db.elephantsql.com/tztgcaku",
     pool: {
       min: 2,
       max: 10,
